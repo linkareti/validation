3374bc49f58a99c8c93a8c1c8df52dafc9eaed87
pzenida <pzenida@linkare.com>
2010-06-16 22:21:12 +0000

    Included the javadoc comments.

--- src/main/java/com/linkare/validation/email/EmailValidator.java
+++ src/main/java/com/linkare/validation/email/EmailValidator.java
@@ -2,14 +2,26 @@ package com.linkare.validation.email;
 
 /**
  * 
+ * Utility class to validate the format of an email address.
+ * 
  * @author Paulo Zenida - Linkare TI
  * 
  */
 public final class EmailValidator {
 
+    /**
+     * Hide utility class constructor. All methods in this class should be accessed statically.
+     */
     private EmailValidator() {
     }
 
+    /**
+     * 
+     * @param email
+     *            the email address whose format we want to validate
+     * @return true if the email format is valid. Returns false otherwise. This method delegates its implementation in the
+     *         <code>org.apache.commons.validator.EmailValidator</code> validator class from the commons-validator project from Apache.
+     */
     public static boolean isValid(final String email) {
 	return org.apache.commons.validator.EmailValidator.getInstance().isValid(email);
     }
